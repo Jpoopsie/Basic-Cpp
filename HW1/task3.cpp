b406b9cba638468045f087d2b4b3f1e54a069a55
Jpoopsie <jpoopsie9@gmail.com>
2024-06-02 16:02:24 +0300

    First commit

---
+++ HW1/task3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "   *   \n"
+			  << "  ***  \n"
+			  << " ***** \n"
+			  << "*******\n"
+			  << " HHOHH\n"
+			  << " ZZZZZ "
+			  << std::endl;
+	return 0;
+}
