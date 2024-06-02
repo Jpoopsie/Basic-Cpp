745f7392f619d25168557b30c0eed26f17a3389f
Jpoopsie <jpoopsie9@gmail.com>
2024-06-02 16:21:41 +0300

    First commit

---
+++ HW2/task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+	int a, b, c, d;
+	std::cin >> a >> b >> c;
+	d = a + b + c;
+	std::cout << a << " + " << b << " + " << c << " = " << d << std::endl;
+	d = a * b * c;
+	std::cout << a << " * " << b << " * " << c << " = " << d << std::endl;
+	return 0;
+}
